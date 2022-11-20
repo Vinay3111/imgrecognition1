5a5164aa288fe207f9a29c655922e798941019dd
Vinay3111 <93260424+Vinay3111@users.noreply.github.com>
2022-11-20 18:02:25 +0530

    Update main.js

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ function take_snapshot()
 
 console.log('ml5 version:', ml5.version);
 
-classifier = ml5.imageClassifier('',modelLoaded);
+classifier = ml5.imageClassifier'https://teachablemachine.withgoogle.com/models/yr1DALi0S/',modelLoaded);
 function modelLoaded() {
     console.log('modelLoaded!');
 }
